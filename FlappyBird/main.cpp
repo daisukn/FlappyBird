5e1be1056ac08e815d9d55306cb37e1f94205fcf
Daisuke <daisukn@outlook.com>
2022-05-07 11:52:21 +0900

    Initial Commit

---
+++ FlappyBird/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FlappyBird
+//
+//  Created by Daisuke Nishimori on 2022/05/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
